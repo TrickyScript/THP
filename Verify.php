c6a698636e88672ece2e6dce5b758b739835d333
TrickyScript <pubgmontage0786@gmail.com>
2022-09-13 16:32:33 +0530

    THP

---
+++ Verify.php
@@ -0,0 +1,443 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title> !! Tricky Hacker Paytm !! </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://i.ibb.co/JqSXcTD/20200519-221251.png" rel="icon" type="image/x-icon"/>
+  <meta name="description" content="online refer bypass script. refer bypass script. task bypass script.">
+  <meta name="author" content="online refer script, refer bypass script, task bypass script.">
+  <meta name="keywords" content="online refer script, refer bypass script, task bypass script.">
+</head>
+<style type="text/css">
+@import url('https://fonts.googleapis.com/css?family=Poppins');
+@import url('https://fonts.googleapis.com/css2?family=Solway&display=swap');
+
+/* BASIC */
+
+*{ 
+ margin: 0;
+ padding: 0;
+ box-sizing: border-box;
+ font-family: "Poppins", sans-serif;
+}
+
+*:focus {
+    outline: none;
+} 
+
+:root { 
+ --main-color: #FF0001;
+ --shadow-color: rgba(93, 38, 193, 0.4);
+}
+
+body { 
+ width: 100%;
+ height: 100vh;
+ background-image: linear-gradient(45deg, var(--main-color) 45%, #ffffff 0%);
+ background-repeat: no-repeat; 
+}
+
+
+/* STRUCTURE  */
+
+.wrapper {
+ width: 100%;
+ min-height: 100%;
+ padding: 80px 20px 20px 20px;
+}
+
+/* ANIMATIONS */
+
+/* Simple CSS3 Fade-in-down Animation */
+.fadeInDown {
+  animation-name: fadeInDown;
+  animation-duration: 2s;
+  animation-fill-mode: both;
+}
+
+@keyframes fadeInDown {
+  0% {
+    opacity: 0;
+    transform: translate3d(0, -100%, 0);
+  }
+  100% {
+    opacity: 1;
+    transform: none;
+  }
+}
+
+
+#formContent {
+  border-radius: 10px;
+  background: #fff;
+  width: 95%;
+  max-width: 400px;
+  min-height: 350px;
+  max-height: 450px;
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
+  box-shadow: 0 30px 60px 0 rgba(0,0,0,0.3);
+}
+
+#navbarId { 
+  text-align: center;   
+}
+
+img {
+  width: 25%;
+  margin: 20px auto 5px auto;
+  padding-top: 30px;
+  display: block;
+}
+
+h2 {
+  text-align: center;
+  font-size: 16px;
+  font-weight: 600;
+  text-transform: uppercase;
+  display: inline-block;
+  letter-spacing: 0.4px;
+  margin: 5px 8px 20px 8px; 
+  color: #636e72;
+  border-bottom: 1px solid #636e72;
+}
+
+
+input[type=text],select {
+  background-color: #f6f6f6;
+  border: none;
+  color: #0d0d0d;
+  padding: 12px 32px;
+  text-align: left;
+  text-decoration: none;
+  display: block;
+  font-size: 14px;
+  margin: 5px auto;
+  width: 85%;
+  border: 2px solid #f6f6f6;
+  -webkit-transition: all 0.5s ease-in-out;
+  -moz-transition: all 0.5s ease-in-out;
+  -ms-transition: all 0.5s ease-in-out;
+  -o-transition: all 0.5s ease-in-out;
+  transition: all 0.5s ease-in-out;
+  -webkit-border-radius: 5px 5px 5px 5px;
+  border-radius: 5px 5px 5px 5px;
+}
+
+input[type=text]:focus {
+  background-color: #fff;
+  border-bottom: 2px solid var(--main-color);
+}
+
+input[type=text]:placeholder {
+  color: #cccccc;
+}
+
+
+/* FORM TYPOGRAPHY*/
+
+input[type=button], input[type=submit], input[type=reset]  {
+  background-color: var(--main-color);
+  border: none;
+  color: white;
+  letter-spacing: 1px;
+  padding: 12px 80px;
+  text-align: center;
+  text-decoration: none;
+  display: block;
+  text-transform: uppercase;
+  font-size: 13px;
+  -webkit-box-shadow: 0 10px 30px 0 var(--shadow-color);
+  box-shadow: 0 10px 30px 0 var(--shadow-color);
+  -webkit-border-radius: 5px 5px 5px 5px;
+  border-radius: 5px 5px 5px 5px;
+  margin: 5px auto 35px auto;
+  -webkit-transition: all 0.3s ease-in-out;
+  -moz-transition: all 0.3s ease-in-out;
+  -ms-transition: all 0.3s ease-in-out;
+  -o-transition: all 0.3s ease-in-out;
+  transition: all 0.3s ease-in-out;
+  cursor: pointer;
+}
+
+input[type=button]:hover, input[type=submit]:hover, input[type=reset]:hover  {
+  background-color: var(--main-color);
+}
+
+input[type=button]:active, input[type=submit]:active, input[type=reset]:active  {
+  -moz-transform: scale(0.95);
+  -webkit-transform: scale(0.95);
+  -o-transform: scale(0.95);
+  -ms-transform: scale(0.95);
+  transform: scale(0.95);
+}
+
+/* Simple CSS3 Fade-in Animation */
+@-webkit-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+@-moz-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+@keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+
+.fadeIn {
+  opacity:0;
+  -webkit-animation:fadeIn ease-in 1;
+  -moz-animation:fadeIn ease-in 1;
+  animation:fadeIn ease-in 1;
+
+  -webkit-animation-fill-mode:forwards;
+  -moz-animation-fill-mode:forwards;
+  animation-fill-mode:forwards;
+
+  -webkit-animation-duration:1s;
+  -moz-animation-duration:1s;
+  animation-duration:1s;
+}
+
+.fadeIn.first {
+  -webkit-animation-delay: 0.4s;
+  -moz-animation-delay: 0.4s;
+  animation-delay: 0.4s;
+}
+
+.fadeIn.second {
+  -webkit-animation-delay: 0.6s;
+  -moz-animation-delay: 0.6s;
+  animation-delay: 0.6s;
+}
+
+.fadeIn.third {
+  -webkit-animation-delay: 0.8s;
+  -moz-animation-delay: 0.8s;
+  animation-delay: 0.8s;
+}
+
+.fadeIn.fourth {
+  -webkit-animation-delay: 1s;
+  -moz-animation-delay: 1s;
+  animation-delay: 1s;
+}
+
+
+#formFooter {
+  background-color: #f6f6f6;
+  border-top: 1px solid #dce8f1;
+  padding: 25px;
+  text-align: center;
+  border-radius: 0 0 10px 10px;
+}
+
+a {
+  color: var(--main-color);
+  display:inline-block;
+  text-decoration: none;
+  font-weight: 400;
+}
+
+/* Simple CSS3 Fade-in Animation */
+.underlineHover:after {
+  display: block;
+  left: 0;
+  bottom: -10px;
+  width: 0;
+  height: 2px;
+  background-color: var(--main-color);
+  content: "";
+  transition: width 0.2s;
+}
+
+.underlineHover:hover {
+  color: #0d0d0d;
+}
+
+.underlineHover:hover:after{
+  width: 100%;
+}
+
+.small_text_msg {
+  margin: 0px 0px 10px 25px;
+  color: #636e72;
+  font-size: 12px;
+  display: block;
+  text-align: left;
+}
+
+.BoxContent {
+  background-color: #f6f6f6;
+  width: 85%;
+  min-height: 30px;
+  padding: 15px;
+  margin: 5px auto 20px auto;
+  border-radius: 5px;
+}
+
+.BoxContent p {
+  padding: 10px 0px;
+  border-top: thin solid #ffffff;
+  text-align: center;
+  font-size: 12px;
+  color: #636e72;
+  font-family: 'Solway', serif;
+}
+
+pre {
+  font-size: 13px;
+}
+
+</style>
+
+</style>
+<body>
+  <div class="wrapper fadeInDown">
+    <div id="formContent">
+      <div class="fadeIn first" id="navbarId">
+        <img src="https://i.ibb.co/x2XSGsT/IMG-20220613-225721-531.jpg" alt="Icon">
+        <h2 class="active">Global Media TASK SCRIPT</h2>
+      </div>
+      
+<?php
+
+
+Error_reporting(0);
+function RandomNumber($length)
+{
+$str="";
+for($i=0;$i<$length;$i++){
+$str.=mt_rand(0,9);
+}
+return $str;
+}
+
+$f = array("9","8","7","6","5","4","3","2","1","0");
+$l = array("Aarya","Agarwal","Ahir","Akela","Arora","Awasthi","Banerjee","Bhaduriya","Bhatt","Chakra","Chakarvarti","Chattarjee","Chaubey","Chaturvedi","Chandravanshi","Chauhan","Choudhary","Dhawal","Dhawan","Deshmukh","Dubey","Dokhle","Gandhi","Gokhil","Ghoshal","Gokul","Gond","Goswami","Gupta","Hooda","Jaat","Jain","Jatara","Jayes","Jugi","Kapoor","Kumar","Kharwar","Kesari","Kohli","Kumhaar","Kurmi","Kushwaha","Lala","Lakhani","Lohar","Lokhande","Malhotra","Marvare","Maurya","Maali","Mauryavanshi","Mehra","Mital","Modi","Naidu","Naveen","Nehru","Patani","Paatil","Pal","Pandey","Paneri","Panjiwan","Pathak","Patel","Pele","Prajapati","Prashad","Prabhu","Raghuwanshi","Raj","Rajput","Raja","Ram","Ramdin","Raydu","Rotle","Roy","Sardar","Seikh","Seth","Sharma","Singh","Singhania","Sukla","Shinghle","Surya","Survanshi","Suryavanshi","Swasthi","Talpade","Talwar","Tahrik","Tilakdhari","Tiwari","Tripathi","Trivedi","Verma","Yadav","Yaduvanshi");
+$fname = $f[mt_rand(0,9)];
+$lname = $l[mt_rand(0,100)];
+$name1 = "$fname $lname";
+
+$mo = RandomNumber(9);
+$name=''.$fname.''.$otp.'';
+$mob=RandomNumber(15);
+$p=RandomNumber(15);
+$o=RandomNumber(21);
+$imei = RandomNumber(16);
+$otp= RandomNumber(3);
+$ses= RandomNumber(14);
+$token1 = RandomNumber(100);
+$PHPSESSID1 = RandomNumber(25);
+$i1 = RandomNumber(5);
+$i2 = RandomNumber(8);
+$i3 = RandomNumber(1);
+$im = RandomNumber(10);
+$token=$_GET['token'];
+$pwd=$_GET['pwd'];
+$mo=$_GET['mo'];
+$id=$_GET['id'];
+
+$url7='https://www.gbmedias.org/api/Common/getonline';
+ 
+$data7='lang=en&token='.$token.'';
+
+
+
+$headers7[]='Host: www.gbmedias.org';
+$headers7[]='Connection: keep-alive';
+$headers7[]='Content-Length: '.strlen($data7).'';
+$headers7[]='user-agent: Mozilla/5.0 (Linux; Android 10; Redmi 8 Build/QKQ1.191014.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/97.0.4692.70 Mobile Safari/537.36 ';
+$headers7[]='X-Requested-With: mark.via.gp';
+$headers7[]='Content-Type: application/x-www-form-urlencoded';
+
+
+$ch77 = curl_init();
+curl_setopt($ch77, CURLOPT_URL,$url7);
+curl_setopt($ch77, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch77, CURLOPT_POSTFIELDS,$data7);
+ curl_setopt($ch77, CURLOPT_RETURNTRANSFER, 1);
+   curl_setopt($ch77, CURLOPT_HTTPHEADER,$headers7);
+ curl_setopt($ch77, CURLOPT_SSL_VERIFYPEER, false);
+ $output66= curl_exec ($ch77);
+ $json=json_decode($output66,true);
+curl_close($ch);
+$a=$json["0"]["msg"];
+$code=$json['code'];
+$order_id=$json['order_id'];
+
+ curl_close($ch);
+ 
+
+?>
+
+<?php
+ 
+$url8="https://www.gbmedias.org/api/task/taskOrderSubmit";
+ 
+$data8='order_id='.$id.'&status=3&lang=en&token='.$token.'';
+
+
+
+$dd=strlen($data8);
+$headers8 = array(
+
+   "Host: www.gbmedias.org",
+
+   "Content-Length: $dd",
+
+   "Accept: application/json, text/plain, */*",
+
+   "User-Agent: Mozilla/5.0 (Linux; Android 9; SM-G977N Build/LMY48Z;) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/101.0.4951.41 Mobile Safari/537.36",
+
+   "Content-Type: application/x-www-form-urlencoded",
+
+   "Origin: https://www.gbmedias.org",
+
+   "X-Requested-With: mark.via.gp",
+
+   "Sec-Fetch-Site: same-origin",
+
+   "Sec-Fetch-Mode: cors",
+
+   "Sec-Fetch-Dest: empty",
+
+   "Referer: https://www.gbmedias.org/",
+
+   "Accept-Encoding: gzip, deflate",
+
+   "Accept-Language: en-US,en;q=0.9",
+
+   "Authorization: Host: www.gbmedias.org Content-Length: 88 Accept: application/json, text/plain, */* User-Agent: Mozilla/5.0 (Linux; Android 9; SM-G977N Build/LMY48Z;) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/101.0.4951.41 Mobile Safari/537.36 Content-Type: application/x-www-form-urlencoded Origin: https://www.gbmedias.org X-Requested-With: mark.via.gp Sec-Fetch-Site: same-origin Sec-Fetch-Mode: cors Sec-Fetch-Dest: empty Referer: https://www.gbmedias.org/ Accept-Encoding: gzip, deflate Accept-Language: en-US,en;q=0.9",
+
+);
+
+
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$url8);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$data8);
+curl_setopt($ch,CURLOPT_HEADER,0);
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+ curl_setopt($ch, CURLOPT_HTTPHEADER,$headers8);
+   curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
+   curl_setopt($ch, CURLOPT_ENCODING, 'gzip');
+
+ $output66= curl_exec ($ch);
+
+
+ curl_close ($ch);
+ $json=json_decode($output66,true);
+curl_close($ch);
+$a=$json["0"]["msg"];
+$code=$json['code'];
+$msg=$json['code_dec'];
+
+ curl_close($ch);
+ 
+ echo "<small class='small_text_msg fadeIn second'>Status - $msg</small>";
+      
+echo "<meta http-equiv=refresh content=1;url=https://trickyhacker.zya.me/Global_Media/Bypass.php?token=$token&mo=&pwd=&submit=Task+Bypass>";  
+ 
+?> 
+
+<div id="formFooter">
+        <a class="underlineHover" href="https://t.me/TrickyHacker_Paytm">Join Telegram Channel?</a>
+      </div>
